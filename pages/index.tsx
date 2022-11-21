54124be0c3a674db3494bb436c16866578548672
leoniefelberd <75806511+leoniefelberd@users.noreply.github.com>
2022-11-21 14:46:23 +0100

    now is first deploy-now on

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           hello new site! new branch is made now
+          if i change here now deploy-now
         </h1>
 
       </main>
